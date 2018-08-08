ed119db43f320512b3e36f649a39cbea3b4aea98
fjc0k <fjc0kb@gmail.com>
2018-08-09 01:09:16 +0800

    add PassSlots

---
+++ src/components/PassSlots/PassSlots.vue
@@ -0,0 +1,15 @@
+<script>
+import { createComponent } from '../_utils'
+
+export default createComponent({
+  name: 'PassSlots',
+
+  functional: true,
+
+  render(h, { parent: { $slots = {} } }) {
+    return Object
+      .keys($slots)
+      .map(slot => h('template', { slot }, $slots[slot]))
+  }
+})
+</script>
