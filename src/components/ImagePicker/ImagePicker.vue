6eb88b96ac35c47d87469bdc82c4adeb39e08888
Joy Fong <fjc0kb@gmail.com>
2018-08-08 18:25:07 +0800

    basic ImagePicker

---
+++ src/components/ImagePicker/ImagePicker.vue
@@ -0,0 +1,22 @@
+<template>
+  <PickerView
+    v-model="localValue"
+    v-bind="$attrs"
+    :data="data"
+    cascaded
+  />
+</template>
+
+<script>
+/* eslint max-nested-callbacks: 0 */
+import { range } from 'lodash'
+import { subYears, addYears, parse, getDaysInMonth } from 'date-fns'
+import { createComponent, dateToArray, formatDate } from '../_utils'
+import PickerView from '../PickerView/PickerView.vue'
+
+const now = new Date()
+
+export default createComponent({
+  name: 'DatePickerView'
+})
+</script>
