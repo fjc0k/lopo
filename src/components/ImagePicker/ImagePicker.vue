4efda663b331e534b1a2b54acd4cf477a7c21152
fjc0k <fjc0kb@gmail.com>
2018-08-20 22:55:33 +0800

    update

--- src/components/ImagePicker/ImagePicker.vue
+++ src/components/ImagePicker/ImagePicker.vue
@@ -25,38 +25,42 @@
             :style="{
               transform: `rotate(${item.rotation}deg)`
             }"
-            @click="handleImageClick(item, (listIndex * cols) + itemIndex)"
+            @click.native="handleImageClick(item, (listIndex * cols) + itemIndex)"
           />
         </template>
       </div>
     </div>
+    <ImageViewer
+      v-model="preview"
+      :data="localValue"
+      :index="clickedImageIndex"
+    />
   </div>
 </template>
 
 <script>
 /* eslint max-nested-callbacks: 0 */
-import { toArray } from 'lodash'
+import { isPlainObject, toArray } from 'lodash'
 import { createComponent, chunk } from '../_utils'
 import { getOrientation, getRotation } from './_utils'
 import Icon from '../Icon/Icon.vue'
 import XImage from '../Image/Image.vue'
+import ImageViewer from '../ImageViewer/ImageViewer.vue'
 
 const IMAGE_SELECTOR = 0
 
 export default createComponent({
   name: 'ImagePicker',
 
-  components: { Icon, XImage },
-
-  model: {
-    prop: 'files',
-    event: 'input'
-  },
+  components: { Icon, XImage, ImageViewer },
 
   props: {
-    files: {
+    value: {
       type: Array,
-      default: () => []
+      default: () => [],
+      transform: files => files.map(file => {
+        return isPlainObject(file) ? file : { url: file }
+      })
     },
     disabled: Boolean,
     multiple: Boolean,
@@ -74,13 +78,17 @@ export default createComponent({
     }
   },
 
-  data: () => ({ IMAGE_SELECTOR }),
+  data: () => ({
+    IMAGE_SELECTOR,
+    clickedImageIndex: 0,
+    preview: false
+  }),
 
   computed: {
     lists() {
       const { cols, disabled } = this
       const items = [
-        ...this.localFiles,
+        ...this.localValue,
         ...(disabled ? [] : [IMAGE_SELECTOR])
       ]
       return chunk(items, cols)
@@ -89,12 +97,12 @@ export default createComponent({
 
   methods: {
     addImage(image) {
-      const { localFiles } = this
-      const files = localFiles.slice()
+      const { localValue } = this
+      const files = localValue.slice()
       files.push(image)
       this.$emit('add', image)
-      this.$emit('change', files, localFiles)
-      this.sendFiles(files)
+      this.$emit('change', files, localValue)
+      this.sendValue(files)
     },
     parseFile(file, index) {
       const reader = new FileReader()
@@ -120,17 +128,19 @@ export default createComponent({
       reader.readAsDataURL(file)
     },
     handleRemoveClick(image, index) {
-      const { localFiles } = this
-      const files = localFiles.slice()
+      const { localValue } = this
+      const files = localValue.slice()
       files.splice(index, 1)
       this.$emit('remove', image, index)
-      this.$emit('change', files, localFiles)
+      this.$emit('change', files, localValue)
       this.sendFiles(files)
     },
     handleInputChange({ target: { files } }) {
       toArray(files).forEach(this.parseFile)
     },
     handleImageClick(image, index) {
+      this.clickedImageIndex = index
+      this.preview = true
       this.$emit('image-click', image, index)
     }
   }
